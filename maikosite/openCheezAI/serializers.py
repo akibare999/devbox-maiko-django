86c71ec9b1317a25b49cfaa92837862f709f533a
Akibare Nanashi <akibare@gmail.com>
2016-02-01 18:56:08 -0600

    Improve form field names in HTML browser API

--- maikosite/openCheezAI/serializers.py
+++ maikosite/openCheezAI/serializers.py
@@ -11,14 +11,28 @@ class PersonSerializer(serializers.HyperlinkedModelSerializer):
         fields = ( 'uin', 
                    'url',
                    'uiuc_netid', 
-                   'uic_netid', 
-                   'uis_netid', 
                    'illinois_netid', 
                    'uillinois_netid', 
+                   'uic_netid', 
+                   'uis_netid', 
                    'i2s_firstname', 
                    'i2s_lastname', 
                    'banner_firstname', 
                    'banner_lastname', 
                    'banner_suppressed',
                   )
+        extra_kwargs = {
+            'uin' : {'label': 'UIN'},
+            'uiuc_netid' : {'label': 'UIUC NetID'},
+            'illinois_netid' : {'label': 'Illinois NetID'},
+            'uillinois_netid' : {'label': 'Enterprise NetID'},
+            'uic_netid' : {'label': 'UIC NetID'},
+            'uis_netid' : {'label': 'UIS NetID'},
+
+            'banner_firstname' : {'label': 'Banner first name'},
+            'banner_lastname' : {'label': 'Banner last name'},
+            'i2s_firstname' : {'label': 'I2S first name'},
+            'i2s_lastname' : {'label': 'I2S last name'},
+            'banner_suppressed' : {'label': 'FERPA suppressed?'},
+        }
 
